fdf590a97fd9b0265af36deff85067679c12ce31
owain-williams <owain@andersdigital.com>
2022-07-01 12:25:36 +0100

    edited get_quote() for clarity, get quote on initialisation

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import requests
 
 def get_quote():
     r = requests.get(url="https://api.kanye.rest/")
-    canvas.itemconfig(quote_text, text=r.json()["quote"])
+    r.raise_for_status()
+    data = r.json()
+    canvas.itemconfig(quote_text, text=data["quote"])
 
 
 window = Tk()
@@ -21,5 +23,7 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
+
+get_quote()
 
 window.mainloop()
